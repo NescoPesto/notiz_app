00fde825106a9a760fa221d1b5d5cda2f11e6488
Pablo Braun <nesco@tu-berlin.de>
2025-05-18 20:40:00 +0200

    kleine Änderungen in listview.dart

--- lib/listview.dart
+++ lib/listview.dart
@@ -8,12 +8,12 @@ class ListenZeiger extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      shrinkWrap: true, // Card wird nur so viel Platz nehmen wie benötigt
+      shrinkWrap: true, // Container wird nur so viel Platz nehmen wie benötigt
       itemCount: notizen.length, //itemCount steht für wie viele Cards werden angezeigt
       itemBuilder: (context, index) { // es wird indexiert gezeigt, dass heißt, dass es einmal pro Notiz passiert
         final notiz = notizen[index]; //dem final notiz wird eine Notiz aus der Liste zugewiesen
         return Container( // In Container-Format
-            color: Color.fromARGB(64, 193, 193, 193),
+            color: Color.fromARGB(86, 170, 170, 170),
             child: ListTile(
             title: Text(notiz['titel']!), //VS-Code erfodert ein 'Nullcheck'
             subtitle: Text(notiz['notiz']!),
